191c683db9d998b129a870861160fcd0d649b30b
wangbei16 <wangbei6@jd.com>
2024-01-24 11:27:17 +0800

    first commit

---
+++ src/pages/index/index.tsx
@@ -0,0 +1,184 @@
+import React, {
+  Children,
+  forwardRef, FunctionComponent,
+  ReactNode,
+  useEffect,
+  useImperativeHandle,
+  useRef,
+  useState,
+} from 'react'
+import {
+  Swiper as TaroSwiper,
+  SwiperItem as TaroSwiperItem,
+  SwiperProps as TaroSwiperProps,
+  View,
+} from '@tarojs/components'
+import classNames from 'classnames'
+import {CommonEventFunction} from '@tarojs/components/types/common'
+
+export interface SwiperProps extends Omit<TaroSwiperProps, 'ref'> {
+  width: number
+  height: number | string
+  direction: 'horizontal' | 'vertical'
+  indicator: ReactNode
+  autoPlay: boolean
+  loop: boolean
+  defaultValue: number
+  onChange: (e: any) => void
+}
+
+const defaultProps = {
+  direction: 'horizontal',
+  indicator: false,
+  autoPlay: false,
+  loop: false,
+  defaultValue: 0,
+} as SwiperProps
+
+const classPrefix = 'nut-swiper'
+export const Swiper = forwardRef((props: Partial<SwiperProps>, ref) => {
+  const {
+    width,
+    height,
+    className,
+    children,
+    indicator,
+    loop,
+    autoPlay,
+    duration,
+    direction,
+    defaultValue,
+    onChange,
+    ...rest
+  } = {
+    ...defaultProps,
+    ...props,
+  }
+  const [current, setCurrent] = useState(defaultValue)
+  const childrenCount = useRef(Children.toArray(children).length)
+  useEffect(() => {
+    // setCurrent(defaultValue)
+  }, [defaultValue])
+
+  const handleOnChange: CommonEventFunction<
+    TaroSwiperProps.onChangeEventDetail
+  > = (value) => {
+    // setCurrent(value.detail.current)
+  }
+  useImperativeHandle(ref, () => ({
+    to: (value: number) => {
+      setCurrent(value)
+    },
+    next: () => {
+      if (loop) {
+        setCurrent((current + 1) % childrenCount.current)
+      } else {
+        setCurrent(current + 1 >= childrenCount.current ? current : current + 1)
+      }
+    },
+    prev: () => {
+      if (loop) {
+        let next = current - 1
+        next = next < 0 ? childrenCount.current + next : next
+        setCurrent(next % childrenCount.current)
+      } else {
+        setCurrent(current - 1 <= 0 ? 0 : current - 1)
+      }
+    },
+  }))
+  return (
+    <View
+      className={classNames(classPrefix, className)}
+      style={{
+        width: '100%',
+        height: '150px',
+      }}
+    >
+      <View
+        className="nut-swiper-inner"
+        style={{
+          width: '100%',
+          height: '150px',
+        }}
+      >
+        <TaroSwiper
+          current={current}
+          circular={loop}
+          autoplay={autoPlay}
+          vertical={direction === 'vertical'}
+          indicatorDots
+          onChange={(e) => {
+            handleOnChange(e)
+            props.onChange?.(e)
+          }}
+          style={{
+            width: '100%',
+            height: '150px',
+          }}
+          {...rest}
+        >
+          {Children.toArray(children).map((child, index) => {
+            let className
+            if (React.isValidElement(child)) {
+              className = child.props.className
+            }
+            return (
+              <TaroSwiperItem className={className} key={index}>
+                {child}
+              </TaroSwiperItem>
+            )
+          })}
+        </TaroSwiper>
+      </View>
+    </View>
+  )
+})
+
+Swiper.defaultProps = defaultProps
+Swiper.displayName = 'NutSwiper'
+
+export interface SwiperItemProps {
+  itemId?: string
+  skipHiddenItemLayout?: boolean
+  children?: any
+}
+
+const defaultProps1 = {
+  itemId: '',
+  skipHiddenItemLayout: false,
+} as SwiperItemProps
+
+export const SwiperItem: FunctionComponent<Partial<SwiperItemProps>> = (
+  props
+) => {
+  const { children, } = props
+  return <>{children}</>
+}
+SwiperItem.defaultProps = defaultProps1
+SwiperItem.displayName = 'NutSwiperItem'
+
+const Index = () => {
+  const list = [
+    'https://storage.360buyimg.com/jdc-article/NutUItaro34.jpg',
+    'https://storage.360buyimg.com/jdc-article/NutUItaro2.jpg',
+    'https://storage.360buyimg.com/jdc-article/welcomenutui.jpg',
+    'https://storage.360buyimg.com/jdc-article/fristfabu.jpg',
+  ]
+  return <>
+    <Swiper defaultValue={1} autoPlay indicator>
+      {list.map((item, index) => (
+        <SwiperItem key={item}>
+          <img
+            width="100%"
+            height="100%"
+            onClick={() => console.log(index)}
+            src={item}
+            alt=""
+          />
+        </SwiperItem>
+      ))}
+    </Swiper>
+  </>
+}
+
+export default Index
